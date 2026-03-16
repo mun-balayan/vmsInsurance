9e1f91134e26ec4d24a98ea317dcd3d0d6b5d7e8
Cambalisa236095 <152066185+Cambalisa236095@users.noreply.github.com>
2026-03-16 09:15:53 +0800

    VMS System Major Update
    Insurance are moved to be standalone web app

---
+++ sw.js
@@ -0,0 +1,35 @@
+// Insurance Monitor Service Worker
+const CACHE = 'vms-ins-v1';  // ← bump this number every time you upload a new index.html
+const ASSETS = [
+  './',
+  './index.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(cached => {
+        const network = fetch(e.request).then(res => {
+          if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+          return res;
+        }).catch(() => cached);
+        return cached || network;
+      })
+    );
+  }
+});
